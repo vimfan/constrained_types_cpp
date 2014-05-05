f24b326572f948a071aaf6dd97d3daf51285e427
Sebastian Madajczyk <vimfan@gmail.com>
2014-05-05 21:29:07 +0200

    Added file constraints.cpp

---
+++ testing/unit_tests/constraints.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file validator.cpp
+ * @author Sebastian Madajczyk, sebastian@madajczyk.net
+ * @date 04.05.2014
+ */
+
+#include <gtest/gtest.h>
+
+#include <ct/constrained_types.hpp>
+
+TEST(Constraints, defaultConstructible)
+{
+}
