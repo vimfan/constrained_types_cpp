5ff682171ce9509214e44c89ceba37970794725e
Sebastian Madajczyk <vimfan@gmail.com>
2014-05-24 23:30:40 +0200

    Further refactoring to avoid code duplications

--- ct/constrained_types.hpp
+++ ct/constrained_types.hpp
@@ -4,115 +4,105 @@
  * @date 04.05.2014
  */
 
-#define COMPOUND_ASSIGNMENT_OPERATOR(TypeName, ValueType, op)     \
-        TypeName& operator op##=(TypeName rhs)                    \
-        {                                                         \
-          value = TypeName (value op rhs.value).value;            \
-          return *this;                                           \
-        }                                                         \
-                                                                  \
-        TypeName& operator op##=(ValueType rhs)                   \
-        { value = TypeName (value op rhs).value;                   \
-          return *this; }                                         \
+#define CT_COMPUND_ASSIGNMENT_OPERATOR(TypeName, ValueType, op) \
+    TypeName& operator op##=(TypeName rhs)                    \
+    {                                                         \
+      value = TypeName (value op rhs.value).value;            \
+      return *this;                                           \
+    }                                                         \
+                                                              \
+    TypeName& operator op##=(ValueType rhs)                   \
+    { value = TypeName (value op rhs).value;                  \
+      return *this;                                           \
+    }                                                         \
 
-#define CONSTRAINED_TYPE(TypeName, ValueType)         \
-    class TypeName                                    \
-    {                                                 \
-    public:                                           \
-        TypeName() : value(0) {}                      \
-                                                      \
-        explicit TypeName(ValueType val)              \
-            : value(val)                              \
-        {}                                            \
-                                                      \
-        TypeName& operator=(const TypeName& rhs)      \
-        {                                             \
-            value = rhs.value;                        \
-            return *this;                             \
-        }                                             \
-                                                      \
-        TypeName& operator=(ValueType val)            \
-        {                                             \
-            value = val;                              \
-            return *this;                             \
-        }                                             \
-                                                      \
-        ValueType get() const { return value; }       \
-                                                      \
-        bool operator==(TypeName rhs) const           \
-        { return rhs.value == value; }                \
-                                                      \
-        bool operator==(ValueType rhs) const          \
-        { return rhs == value; }                      \
-                                                      \
-        bool operator!=(TypeName rhs) const           \
-        { return !(*this == rhs); }                   \
-                                                      \
-        bool operator!=(ValueType rhs) const          \
-        { return value != rhs; }                      \
-                                                      \
-        ValueType operator+(TypeName rhs) const       \
-        { return value + rhs.get(); }                 \
-                                                      \
-        ValueType operator+(ValueType rhs) const      \
-        { return value + rhs; }                       \
-                                                      \
-        ValueType operator*(TypeName rhs) const       \
-        { return value * rhs.get(); }                 \
-                                                      \
-        ValueType operator*(ValueType rhs) const      \
-        { return value * rhs; }                       \
-                                                      \
-        ValueType operator/(TypeName rhs) const       \
-        { return value / rhs.get(); }                 \
-                                                      \
-        ValueType operator/(ValueType rhs) const      \
-        { return value / rhs; }                       \
-                                                      \
-        ValueType operator%(TypeName rhs) const       \
-        { return value % rhs.get(); }                 \
-                                                      \
-        ValueType operator%(ValueType rhs) const      \
-        { return value % rhs; }                       \
-                                                      \
-        TypeName& operator++()                        \
-        { ++value; return *this; }                    \
-                                                      \
-        TypeName operator++(int)                      \
-        { return TypeName(value++); }                 \
-                                                      \
-        TypeName& operator--()                        \
-        { --value; return *this; }                    \
-                                                      \
-        TypeName operator--(int)                      \
-        { return TypeName(value--); }                 \
-                                                      \
-        COMPOUND_ASSIGNMENT_OPERATOR(TypeName, ValueType, +) \
-        COMPOUND_ASSIGNMENT_OPERATOR(TypeName, ValueType, -) \
-                                                      \
-    private:                                          \
-                                                      \
-        ValueType value;                              \
-    };                                                \
-                                                      \
-    inline ValueType operator/(ValueType p1, TypeName p2) \
-    { return p1 / p2.get(); }                             \
-                                                          \
-    inline bool operator==(ValueType p1, TypeName p2)     \
-    { return p1 == p2.get(); }                            \
-                                                          \
-    inline bool operator!=(ValueType p1, TypeName p2)     \
-    { return p1 != p2.get(); }                            \
-                                                          \
-    inline ValueType operator+(ValueType p1, TypeName p2) \
-    { return p1 + p2.get(); }                             \
-                                                          \
-    inline ValueType operator*(ValueType p1, TypeName p2) \
-    { return p1 * p2.get(); }                             \
-                                                          \
-    inline ValueType operator%(ValueType p1, TypeName p2) \
-    { return p1 % p2.get(); }                             \
-                                                          \
+
+#define CT_BINARY_OPERATOR(TypeName, ValueType, op)              \
+    ValueType operator op (TypeName rhs) const                \
+    { return value op rhs.get(); }                            \
+                                                              \
+    ValueType operator op(ValueType rhs) const                \
+    { return value op rhs; }                                  \
+
+
+#define CT_BINARY_OPERATOR_EXT(TypeName, ValueType, op)          \
+    inline ValueType operator op(ValueType p1, TypeName p2)   \
+    {                                                         \
+        return p1 op p2.get();                                \
+    }                                                         \
+
+
+#define CT_RELATIONAL_OPERATOR(TypeName, ValueType, op)          \
+    bool operator op (TypeName rhs) const                     \
+    { return rhs.value op value; }                            \
+                                                              \
+    bool operator op (ValueType rhs) const                    \
+    { return rhs op value; }                                  \
+
+#define CT_RELATIONAL_OPERATOR_EXT(TypeName, ValueType, op)      \
+    inline bool operator op (ValueType p1, TypeName p2)       \
+    { return p1 op p2.get(); }                                \
+
+
+#define CONSTRAINED_TYPE(TypeName, ValueType)                \
+    class TypeName                                           \
+    {                                                        \
+    public:                                                  \
+        TypeName() : value(0) {}                             \
+                                                             \
+        explicit TypeName(ValueType val)                     \
+            : value(val)                                     \
+        {}                                                   \
+                                                             \
+        TypeName& operator=(const TypeName& rhs)             \
+        {                                                    \
+            value = rhs.value;                               \
+            return *this;                                    \
+        }                                                    \
+                                                             \
+        TypeName& operator=(ValueType val)                   \
+        {                                                    \
+            value = val;                                     \
+            return *this;                                    \
+        }                                                    \
+                                                             \
+        ValueType get() const { return value; }              \
+                                                             \
+        CT_RELATIONAL_OPERATOR(TypeName, ValueType, ==)         \
+        CT_RELATIONAL_OPERATOR(TypeName, ValueType, !=)         \
+                                                             \
+        CT_BINARY_OPERATOR(TypeName, ValueType, +)              \
+        CT_BINARY_OPERATOR(TypeName, ValueType, *)              \
+        CT_BINARY_OPERATOR(TypeName, ValueType, /)              \
+        CT_BINARY_OPERATOR(TypeName, ValueType, %)              \
+                                                             \
+        TypeName& operator++()                               \
+        { ++value; return *this; }                           \
+                                                             \
+        TypeName operator++(int)                             \
+        { return TypeName(value++); }                        \
+                                                             \
+        TypeName& operator--()                               \
+        { --value; return *this; }                           \
+                                                             \
+        TypeName operator--(int)                             \
+        { return TypeName(value--); }                        \
+                                                             \
+        CT_COMPUND_ASSIGNMENT_OPERATOR(TypeName, ValueType, +) \
+        CT_COMPUND_ASSIGNMENT_OPERATOR(TypeName, ValueType, -) \
+                                                             \
+    private:                                                 \
+        ValueType value;                                     \
+    };                                                       \
+                                                             \
+    CT_RELATIONAL_OPERATOR_EXT(TypeName, ValueType, ==)         \
+    CT_RELATIONAL_OPERATOR_EXT(TypeName, ValueType, !=)         \
+                                                             \
+    CT_BINARY_OPERATOR_EXT(TypeName, ValueType, /)              \
+    CT_BINARY_OPERATOR_EXT(TypeName, ValueType, +)              \
+    CT_BINARY_OPERATOR_EXT(TypeName, ValueType, *)              \
+    CT_BINARY_OPERATOR_EXT(TypeName, ValueType, %)              \
+                                                             \
     struct DUMMY_##TypeName {}
 
 namespace ct
